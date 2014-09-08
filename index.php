7891fd3658a4e256707de6379dbf4e02e63edbb6
Florian Gourmelen <florian@scsi.nc>
2014-09-08 17:03:05 +1100

    added instagram

--- index.php
+++ index.php
@@ -397,7 +397,15 @@ $_GET['showHowMany'] = '100';
         	<div class="contact-details">
         		<h3><?php echo $contact_details; ?></h3>
                 <ul>
-                    <li><span class="color-text"><?php echo $contact_email; ?></span></li>
+                    <li><span class="color-text"><?php echo $contact_email; ?>
+					<style>.ig-b- { display: inline-block; }
+							.ig-b- img { visibility: hidden; }
+							.ig-b-:hover { background-position: 0 -60px; } .ig-b-:active { background-position: 0 -120px; }
+							.ig-b-v-24 { width: 137px; height: 24px; background: url(//badges.instagram.com/static/images/ig-badge-view-sprite-24.png) no-repeat 0 0; }
+							@media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {
+							.ig-b-v-24 { background-image: url(//badges.instagram.com/static/images/ig-badge-view-sprite-24@2x.png); background-size: 160px 178px; } }</style>
+							<a href="http://instagram.com/matalaapolynesianart?ref=badge" class="ig-b- ig-b-v-24"><img src="//badges.instagram.com/static/images/ig-badge-view-24.png" alt="Instagram" /></a>
+					</span></li>
                     <li>
                     	<?php echo $contact_city; ?>
                     </li>
